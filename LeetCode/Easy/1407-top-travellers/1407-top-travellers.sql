6e3a9ab1b8c1d1f961c714420c3e76af2df3c5e4
ChengyiLi98 <142965074+ChengyiLi98@users.noreply.github.com>
2025-10-19 16:19:59 -0700

    Easy .sql

--- LeetCode/Easy/1407-top-travellers/1407-top-travellers.sql
+++ LeetCode/Easy/1407-top-travellers/1407-top-travellers.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 select name, 
-COALESCE(SUM(Rides.distance), 0) travelled_distance  # provide a default or fallback value when NULL values are encountered in the data
+IFNULL(SUM(Rides.distance), 0) travelled_distance  # provide a default or fallback value when NULL values are encountered in the data,or use IFNULL
 from Users
 left join Rides  # 确保即便某些用户没有任何乘车记录也能出现在结果
 on Users.id = Rides.user_id
